1b484fbe0cd9c9043ce4141c2e7b6c54d6c61c2c
Jay Anand <jay015341@gmail.com>
2024-07-26 23:33:16 +0530

    Initial commit

---
+++ src/SimpleStorage.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.19;
+
+contract SimpleStorage {
+    uint256 public favouriteNumber;
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+    struct People {
+        string name;
+        uint256 favouriteNumber;
+    }
+
+    People[] public person;
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        person.push(People(_name, _favoriteNumber));
+        nameToFavouriteNumber[_name] = _favoriteNumber;
+    }
+
+    function store(uint256 _favoriteNumber) public {
+        favouriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favouriteNumber;
+    }
+}
